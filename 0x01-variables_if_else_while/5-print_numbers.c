5c4c105196703274711f9236c93881d57aa5829e
PeterLangat <36981131+PeterLangat@users.noreply.github.com>
2023-02-17 08:41:15 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	for (; n <= 10; n++)
+		printf("%d", n);
+	putchar('\n');
+	return (0);
+}
+
