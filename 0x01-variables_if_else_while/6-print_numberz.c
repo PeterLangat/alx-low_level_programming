560c5c511448708906c3b2ade093f9a4245551a5
PeterLangat <36981131+PeterLangat@users.noreply.github.com>
2023-02-17 08:51:37 -0800

    6-print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
