104ad1427a0a0556ba470190b8630f984d63a9b0
PeterLangat <36981131+PeterLangat@users.noreply.github.com>
2023-02-27 07:01:54 -0800

    Added 0-main.c 0-reste_to_98.c main.h and README.md files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Takes apointer of an integer
+ * and updates the value it points to to 98
+ *
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
