43e96eb393e8a672f36ec9cbde1397511bd786bd
PeterLangat <36981131+PeterLangat@users.noreply.github.com>
2023-03-10 11:21:46 -0800

    Created a static libray

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - Function to print a string
+ *
+ * @str - input
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
